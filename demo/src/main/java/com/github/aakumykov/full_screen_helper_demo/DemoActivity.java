5dc176d882d134f1048a61844ed56aefa29db476
Андрей Кумыков <aakumykov@yandex.ru>
2023-08-30 12:35:26 +0300

    Улучшил пример, удалил меню, добавил перевод на английский.

--- demo/src/main/java/com/github/aakumykov/full_screen_helper_demo/DemoActivity.java
+++ demo/src/main/java/com/github/aakumykov/full_screen_helper_demo/DemoActivity.java
@@ -1,8 +1,6 @@
 package com.github.aakumykov.full_screen_helper_demo;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.DimenRes;
@@ -27,12 +25,12 @@ public class DemoActivity extends AppCompatActivity {
 
     public static final String KEY_IS_FITS_SYSTEM_BARS = "IS_FITS_SCREEN";
     public static final String KEY_IS_FULL_SCREEN = "IS_FULL_SCREEN";
-    private static final String KEY_FRAGMENT_NUM = "FRAGMENT_NUM";
+    private static final String KEY_FRAGMENT_TAG = "FRAGMENT_NUM";
     private ActivityDemoBinding mBinding;
     private FullScreenController mFullScreenController;
     private FullScreenHelper mFullScreenHelper;
     private FragmentManager mFragmentManager;
-    private int mCurrentFragmentNumber;
+    @Nullable private Fragment mCurrentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +38,15 @@ public class DemoActivity extends AppCompatActivity {
 
         mBinding = ActivityDemoBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+
         setSupportActionBar(mBinding.toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
-        mBinding.floatingActionButton.setOnClickListener(v -> loadOtherFragment());
+
+        mBinding.backButton.setOnClickListener(v -> returnPreviousFragment());
+        mBinding.forwardButton.setOnClickListener(v -> loadNextFragment());
 
         mFullScreenController = new FullScreenController(this);
-//        mFullScreenController.unfitFromSystemBars();
         mFullScreenController.getFullScreenState().observe(this, this::onFullScreenStateChanged);
 
         mFullScreenHelper = new FullScreenHelper(KEY_IS_FITS_SYSTEM_BARS, KEY_IS_FULL_SCREEN, mFullScreenController);
@@ -57,6 +57,7 @@ public class DemoActivity extends AppCompatActivity {
             @Override
             public void onFragmentResumed(@NonNull FragmentManager fm, @NonNull Fragment f) {
                 super.onFragmentResumed(fm, f);
+                mCurrentFragment = f;
                 processFragmentTitle(f);
                 processFragmentFullScreenFlag(f, savedInstanceState);
             }
@@ -65,18 +66,22 @@ public class DemoActivity extends AppCompatActivity {
 
         if (null == savedInstanceState)
             loadFirstFragment();
-        else
-        if (1 == savedInstanceState.getInt(KEY_FRAGMENT_NUM))
-            loadFirstFragment();
-        else
-            loadSecondFragment();
+        else {
+            if (FirstFragment.class.getSimpleName().equals(savedInstanceState.getString(KEY_FRAGMENT_TAG)))
+                loadFirstFragment();
+            else
+                loadSecondFragment();
+        }
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
+
         mFullScreenHelper.onSaveInstanceState(outState);
-        outState.putInt(KEY_FRAGMENT_NUM, mCurrentFragmentNumber);
+
+        if (null != mCurrentFragment)
+            outState.putString(KEY_FRAGMENT_TAG, mCurrentFragment.getClass().getSimpleName());
     }
 
     @Override
@@ -84,22 +89,6 @@ public class DemoActivity extends AppCompatActivity {
         loadFirstFragment();
         return true;
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (R.id.actionSecondFragment == item.getItemId()) {
-            loadSecondFragment();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 
     private void processFragmentTitle(Fragment fragment) {
         if (fragment instanceof HasCustomTitle)
@@ -119,13 +108,21 @@ public class DemoActivity extends AppCompatActivity {
 
         if (fullScreenState.isFitsSystemWindows || fullScreenState.isFullScreen) {
             ViewMarginsAdjustmentHelper.adjustTopMarginTo(mBinding.toolbar, getDimen(R.dimen.fab_margin));
-            ViewMarginsAdjustmentHelper.adjustBottomMarginTo(mBinding.floatingActionButton, getDimen(R.dimen.fab_margin));
-            ViewMarginsAdjustmentHelper.adjustRightMarginTo(mBinding.floatingActionButton, getDimen(R.dimen.fab_margin));
+
+            ViewMarginsAdjustmentHelper.adjustBottomMarginTo(mBinding.forwardButton, getDimen(R.dimen.fab_margin));
+            ViewMarginsAdjustmentHelper.adjustRightMarginTo(mBinding.forwardButton, getDimen(R.dimen.fab_margin));
+
+            ViewMarginsAdjustmentHelper.adjustBottomMarginTo(mBinding.backButton, getDimen(R.dimen.fab_margin));
+            ViewMarginsAdjustmentHelper.adjustLeftMarginTo(mBinding.backButton, getDimen(R.dimen.fab_margin));
         }
         else {
             ViewMarginsAdjustmentHelper.adjustTopMarginToInsets(mBinding.toolbar, fullScreenState.insets, 0);
-            ViewMarginsAdjustmentHelper.adjustBottomMarginToInsets(mBinding.floatingActionButton, fullScreenState.insets, getDimen(R.dimen.fab_margin));
-            ViewMarginsAdjustmentHelper.adjustRightMarginToInsets(mBinding.floatingActionButton, fullScreenState.insets, getDimen(R.dimen.fab_margin));
+
+            ViewMarginsAdjustmentHelper.adjustBottomMarginToInsets(mBinding.forwardButton, fullScreenState.insets, getDimen(R.dimen.fab_margin));
+            ViewMarginsAdjustmentHelper.adjustRightMarginToInsets(mBinding.forwardButton, fullScreenState.insets, getDimen(R.dimen.fab_margin));
+
+            ViewMarginsAdjustmentHelper.adjustBottomMarginToInsets(mBinding.backButton, fullScreenState.insets, getDimen(R.dimen.fab_margin));
+            ViewMarginsAdjustmentHelper.adjustLeftMarginToInsets(mBinding.backButton, fullScreenState.insets, getDimen(R.dimen.fab_margin));
         }
 
         if (fullScreenState.isFullScreen) {
@@ -149,21 +146,20 @@ public class DemoActivity extends AppCompatActivity {
     }
 
 
-    private void loadOtherFragment() {
-        if (1 == mCurrentFragmentNumber)
+    private void loadNextFragment() {
+        if (mCurrentFragment instanceof FirstFragment)
             loadSecondFragment();
-        else
-            loadFirstFragment();
     }
 
+    private void returnPreviousFragment() {
+        mFragmentManager.popBackStack();
+    }
 
     private void loadFirstFragment() {
-        mCurrentFragmentNumber = 1;
         setFragment(FirstFragment.create());
     }
 
     private void loadSecondFragment() {
-        mCurrentFragmentNumber = 2;
         loadFragment(SecondFragment.create());
     }
 
